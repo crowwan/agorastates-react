00af4ea31ee0db31291e8a6968393a5a8dd33aed
crowwan <crowwan@naver.com>
2023-02-03 22:45:54 +0900

    feat: 태그 데이터 추가

---
+++ src/data/filterBy.js
@@ -0,0 +1,34 @@
+export const filterBy = [
+  {
+    name: "html",
+    url: "./asset/html-5.png",
+  },
+  {
+    name: "css",
+    url: "./asset/css-3.png",
+  },
+  {
+    name: "javascript",
+    url: "./asset/js.png",
+  },
+  {
+    name: "node.js",
+    url: "./asset/nodejs.png",
+  },
+  {
+    name: "react.js",
+    url: "./asset/reactjs.png",
+  },
+  {
+    name: "linux",
+    url: "./asset/linux.png",
+  },
+  {
+    name: "git",
+    url: "./asset/git.png",
+  },
+  {
+    name: "unanswered",
+    url: "",
+  },
+];

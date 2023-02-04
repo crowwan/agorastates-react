4e54aa048be22ee04e06d6e29d4340f83a739eb2
crowwan <crowwan@naver.com>
2023-02-05 00:17:08 +0900

    feat: FilterItem 컴포넌트 전달 인자 변경
    Aside 컴포넌트 변경에 따른 전달 인자 변경
    name 추가
    selected 추가
    onFilterClick추가

--- src/components/FilterContainer.jsx
+++ src/components/FilterContainer.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import FilterItem from "./FilterItem";
-function FilterContainer({ title, list }) {
+function FilterContainer({ title, list, filterBy, target, onFilterClick }) {
   return (
     <ul className="filter__filterContainer">
       <span>{title}</span>
       {list.map((a) => (
-        <FilterItem key={a.name} filterBy={a.name} img={a.url} />
+        <FilterItem
+          key={a.name}
+          filterBy={filterBy}
+          name={a.name}
+          img={a.url}
+          onFilterClick={onFilterClick}
+          selected={target === a.name ? "selected" : ""}
+        />
       ))}
     </ul>
   );

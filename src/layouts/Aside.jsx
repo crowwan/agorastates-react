a61b2e0b601aed6247f72c58d654406d32bbc121
crowwan <crowwan@naver.com>
2023-02-05 00:15:07 +0900

    feat: FilterContainer 컴포넌트 전달 인자 변경
    Main 컴포넌트 변경에 따른 전달 인자 변경
    fiterBy => tags, unanswered
    target => query 속성
    onFilterClick 추가

--- src/layouts/Aside.jsx
+++ src/layouts/Aside.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FilterContainer from "../components/FilterContainer";
 import { filterBy } from "../data/filterBy";
+
 const tags = filterBy.filter((a) => a.name !== "unanswered");
 const unanswered = filterBy.filter((a) => a.name === "unanswered");
 
-function Aside({ disabled }) {
+function Aside({ disabled, query, setQueryObj }) {
+  console.log(query);
   return (
     <aside className="filter__container">
       <button className="newDiscussion-btn btn can-disable" disabled={disabled}>
         NEW DISCUSSION
       </button>
-      <FilterContainer title="TAGS" list={tags} />
-      <FilterContainer title="FILTER BY" list={unanswered} />
+      <FilterContainer
+        title="TAGS"
+        list={tags}
+        filterBy={"tag"}
+        target={query.tag || ""}
+        onFilterClick={setQueryObj}
+      />
+      <FilterContainer
+        title="FILTER BY"
+        list={unanswered}
+        filterBy={"unanswered"}
+        target={query.unanswered || ""}
+        onFilterClick={setQueryObj}
+      />
     </aside>
   );
 }

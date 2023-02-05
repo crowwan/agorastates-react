b8953f9280fb96e5cc89e8dac1f72579e179e81d
crowwan <crowwan@naver.com>
2023-02-05 20:18:24 +0900

    feat: 디스커션 나열 기능 추가
    fetch로 받아온 데이터 나열 기능 구현 완료
    페이지네이션 적용 완료
    필터링 구현 완료

--- src/components/DiscussionItem.jsx
+++ src/components/DiscussionItem.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
-function DiscussionItem() {
-  return <div>DiscussionItem</div>;
+import toStringByFormatting from "../utils/toStringFormat";
+function DiscussionItem({ avatarUrl, title, author, createdAt, answer }) {
+  return (
+    <li className="discussions">
+      <img src={avatarUrl} className="discussions__tagImage" alt="avatar" />
+      <div className="discussions__contentBox">
+        <div className="discussions_title">{title}</div>
+        <div className="discussions__infoBox">
+          <div className="discussions__author">{author}</div>
+          <div className="discussions__createAt">
+            {toStringByFormatting(new Date(createdAt))}
+          </div>
+        </div>
+      </div>
+      <div
+        className={`discussions__answered
+      ${answer ? "answered" : ""}
+    `}
+      >
+        Answered
+      </div>
+    </li>
+  );
 }
 
 export default DiscussionItem;
--- src/components/DiscussionsSection.jsx
+++ src/components/DiscussionsSection.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-
-function DiscussionsSection({ discussions = [], setQueryObj }) {
-  console.log(discussions);
+import DiscussionItem from "./DiscussionItem";
+import Pagination from "../layouts/Pagination";
+function DiscussionsSection({ discussions = [], pages, page, setQueryObj }) {
   return (
-    <div>
-      {discussions.map((a) => (
-        <>{a.id}</>
-      ))}
-    </div>
+    <section id="discussion__wrapper">
+      <ul className="discussions__container">
+        {discussions.map((a) => (
+          <DiscussionItem
+            key={a.id}
+            title={a.title}
+            author={a.author}
+            avatarUrl={a.avatarUrl}
+            createdAt={a.createdAt}
+            answer={a.answer}
+          />
+        ))}
+      </ul>
+      <Pagination pages={pages} page={page} setQueryObj={setQueryObj} />
+    </section>
   );
 }
 

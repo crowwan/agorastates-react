2b1912fbb96bf9f4072c46411685cf17e5790da5
crowwan <crowwan@naver.com>
2023-02-05 20:12:24 +0900

    feat: PageButton 컴포넌트 props 변경 및 페이지 버튼 클릭 핸들러 구현
    key props 추가, 클릭 이벤트 핸들러 구현

--- src/layouts/Pagination.jsx
+++ src/layouts/Pagination.jsx
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import PageButton from "../components/PageButton";
 
-function Pagination({ pages, page }) {
+function Pagination({ pages, page, setQueryObj }) {
   const pageList = Array(pages)
     .fill()
     .map((_, i) => i);
+  const onPageClick = (e) => {
+    setQueryObj((prev) =>
+      prev.page === +e.target.textContent
+        ? prev
+        : { ...prev, page: +e.target.textContent }
+    );
+  };
 
   return (
     <div className="pagination">
       <ul className="pageList">
         {pageList.map((_, i) => (
-          <PageButton i={i} page={page} />
+          <PageButton key={i} i={i} page={page} onClick={onPageClick} />
         ))}
       </ul>
     </div>

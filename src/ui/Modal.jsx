afcc457d9621a200aded2ebc2219740c04a3ce20
crowwan <crowwan@naver.com>
2023-02-05 20:08:50 +0900

    feat: Header 컴포넌트 모달 구현으로 인한 코드 변경
    Header 컴포넌트 모달 기능 구현으로 코드 추가 및 수정

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,15 @@
 import React from "react";
 
-export default function Modal() {}
+export default function Modal({ title, children, setShow }) {
+  const onModalClick = (e) => {
+    if (e.target.classList.contains("modal")) setShow("");
+  };
+  return (
+    <section className="modal" onClick={onModalClick}>
+      <div className="modal__container">
+        <header className="modal__title">{title}</header>
+        {children}
+      </div>
+    </section>
+  );
+}

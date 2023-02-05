f5d1addfeb46423e39c8abd09f19d03d5cf7a8b9
crowwan <crowwan@naver.com>
2023-02-05 20:06:36 +0900

    feat: Header, Main 컴포넌트 props 추가
    유저 로그인 구현에 따른 컴포넌트 props 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "./layouts/Header";
 import Main from "./layouts/Main";
 import { storageAPI } from "./storage/storageAPI.js";
 function App() {
+  const [user, setUser] = useState("");
+
   useEffect(() => {
     storageAPI.setInitialStorage();
   }, []);
@@ -10,8 +12,8 @@ function App() {
     <div className="App">
       <div id="wrapper">
         <div className="container">
-          <Header />
-          <Main />
+          <Header user={user} setUser={setUser} />
+          <Main user={user} />
         </div>
       </div>
     </div>

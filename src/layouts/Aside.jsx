f61f2fd07314451fb8dcfc2bbf9b06f57eec09d1
crowwan <crowwan@naver.com>
2023-02-05 21:44:26 +0900

    feat: 새 디스커션 추가 기능 구현 완료
    새 디스커션 추가 기능을 위한 모달 구현 완료
    fetch 요청 구현 완료 및 실제 반영 확인 완료

--- src/layouts/Aside.jsx
+++ src/layouts/Aside.jsx
@@ -1,15 +1,94 @@
 import React, { useEffect, useState } from "react";
 import FilterContainer from "../components/FilterContainer";
 import { filterBy } from "../data/filterBy";
-
+import Modal from "../ui/Modal";
+import ModalContent from "../components/ModalContent";
 const tags = filterBy.filter((a) => a.name !== "unanswered");
 const unanswered = filterBy.filter((a) => a.name === "unanswered");
 
-function Aside({ disabled, query, setQueryObj }) {
-  console.log(query);
+const DiscussionModal = ({ user, title, setShow, setDiscussions }) => {
+  const [discussionTitle, setDiscussionTitle] = useState("");
+  const [tag, setTag] = useState("");
+  const [content, setContent] = useState("");
+  const [submit, setSubmit] = useState(false);
+
+  const onTitleChange = (e) => {
+    setDiscussionTitle(e.target.value);
+  };
+  const onTagChange = (e) => {
+    setTag(e.target.value);
+  };
+  const onContentChange = (e) => {
+    setContent(e.target.value);
+  };
+  const onSubmitClick = () => {
+    setSubmit((prev) => !prev);
+  };
+  useEffect(() => {
+    submit &&
+      (async () => {
+        const res = await fetch("http://localhost:4000/discussions?page=1", {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: Date.now().toString(),
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+            title: discussionTitle,
+            author: user,
+            bodyHTML: content,
+            tag: tag,
+            avatarUrl: "./asset/user.png",
+            answer: null,
+          }),
+        }).then((res) => res.json());
+
+        setDiscussions(res.discussion);
+        setShow((prev) => !prev);
+      })();
+  }, [submit]);
+  return (
+    <Modal title={title} setShow={setShow}>
+      <ModalContent text="TITLE">
+        <input
+          className="modal__body new-discussion-title"
+          onChange={onTitleChange}
+        />
+      </ModalContent>
+      <ModalContent text="TAG">
+        <input
+          className="modal__body new-discussion-tag"
+          onChange={onTagChange}
+        />
+      </ModalContent>
+      <ModalContent text="CONTENT">
+        <textarea
+          className="modal__body new-discussion-content"
+          onChange={onContentChange}
+        />
+      </ModalContent>
+      <button className="modal__btnList--btn btn" onClick={onSubmitClick}>
+        SUBMIT
+      </button>
+    </Modal>
+  );
+};
+
+function Aside({ disabled, query, user, setQueryObj, setDiscussions }) {
+  const [show, setShow] = useState(false);
+  const onBtnClick = () => {
+    setShow((prev) => !prev);
+  };
   return (
     <aside className="filter__container">
-      <button className="newDiscussion-btn btn can-disable" disabled={disabled}>
+      <button
+        className="newDiscussion-btn btn can-disable"
+        onClick={onBtnClick}
+        disabled={disabled}
+      >
         NEW DISCUSSION
       </button>
       <FilterContainer
@@ -26,6 +105,14 @@ function Aside({ disabled, query, setQueryObj }) {
         target={query.unanswered || ""}
         onFilterClick={setQueryObj}
       />
+      {show && (
+        <DiscussionModal
+          user={user}
+          title="NEW DISCUSSION"
+          setShow={setShow}
+          setDiscussions={setDiscussions}
+        />
+      )}
     </aside>
   );
 }

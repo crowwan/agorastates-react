f1fa5239b0013393c679b6657c0da40a892fa6e1
crowwan <crowwan@naver.com>
2023-02-05 20:08:06 +0900

    feat: Observer pattern 미사용으로 인한 코드 제거
    리액트로 전환함에 따라 observer pattern을 사용하지 않기 때문에 코드 주석 처리

--- src/user/userAPI.js
+++ src/user/userAPI.js
@@ -1,8 +1,8 @@
 import storage from "../storage/storage.js";
 import SHA256 from "../utils/hash.js";
-import { user } from "./user.js";
+
 export const userAPI = {
-  signUp(userId, userPw, url = "./asset/user.png") {
+  signUp(userId, userPw) {
     const userData = storage.getData("user");
 
     if (userData[userId]) {
@@ -10,10 +10,14 @@ export const userAPI = {
       return false;
     }
 
-    userData[userId] = { id: userId, pw: SHA256(userPw), url: url };
+    userData[userId] = {
+      id: userId,
+      pw: SHA256(userPw),
+      url: "./asset/user.png",
+    };
     storage.setData("user", userData);
-    user.setCurrentUser(userId);
-    user.notifyAll();
+    // user.setCurrentUser(userId);
+    // user.notifyAll();
     return true;
   },
 
@@ -21,8 +25,8 @@ export const userAPI = {
     const userData = storage.getData("user");
     console.log(userId, userPw);
     if (userData[userId] && userData[userId].pw === SHA256(userPw)) {
-      user.setCurrentUser(userId);
-      user.notifyAll();
+      // user.setCurrentUser(userId);
+      // user.notifyAll();
       return true;
     } else if (!userData[userId]) alert("아이디가 존재하지 않습니다.");
     else if (!passwordMatch(userData[userId], SHA256(userPw)))
@@ -31,8 +35,8 @@ export const userAPI = {
     return false;
   },
   logOut() {
-    user.setCurrentUser("");
-    user.notifyAll();
+    // user.setCurrentUser("");
+    // user.notifyAll();
   },
 };
 

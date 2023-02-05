02964b77d7395d3092b4818c9160b4460843b266
crowwan <crowwan@naver.com>
2023-02-05 20:13:11 +0900

    feat: props 변경에 따른 코드 수정
    클릭 핸들러 추가

--- src/components/PageButton.jsx
+++ src/components/PageButton.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function PageButton(i, page) {
-  return <li className={i + 1 === page ? "now" : ""}>{i + 1}</li>;
+function PageButton({ i, page, onClick }) {
+  return (
+    <li className={i + 1 === page ? "now" : ""} onClick={onClick}>
+      {i + 1}
+    </li>
+  );
 }
 
 export default PageButton;

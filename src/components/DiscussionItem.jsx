3f86655572a5d06ea907781f4302d9d5673cd005
crowwan <crowwan@naver.com>
2023-02-06 18:17:15 +0900

    feat: 클릭 이벤트 구현
    클릭 핸들러 연결 완료

--- src/components/DiscussionItem.jsx
+++ src/components/DiscussionItem.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import toStringByFormatting from "../utils/toStringFormat";
-function DiscussionItem({ avatarUrl, title, author, createdAt, answer }) {
+function DiscussionItem({
+  avatarUrl,
+  title,
+  author,
+  createdAt,
+  answer,
+  onListClick,
+}) {
+  //TODO: 디스커션 아이템 클릭 시 모달 등장 -> answer ? answerbtn x : answerbtn o / user === createduser ? removebtn o : x
+  //TODO: Answer 버튼 및 삭제 버튼 구현 -> fetch patch, fetch delete
+
   return (
-    <li className="discussions">
+    <li className="discussions" onClick={onListClick}>
       <img src={avatarUrl} className="discussions__tagImage" alt="avatar" />
       <div className="discussions__contentBox">
         <div className="discussions_title">{title}</div>

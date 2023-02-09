fcdd6d2b7df12d720596ef179693d050de6d22cc
crowwan <crowwan@naver.com>
2023-02-09 14:42:16 +0900

    feat: fetchDataWithQuery, fetchDataById 함수 추가
    요청 시 쿼리스트링을 포함한 요청 함수 추가
    기존 함수는 사용하지 않지만 남겨놓음
    디스커션 아이디로 데이터 불러오는 요청에 대한 함수 추가,

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,10 +7,18 @@ const fetchData = (path, query) =>
       )
     : fetch(`${url}/${path}`).then((res) => res.json());
 
+const fetchDataWithQuery = (path, query) => {
+  const qs = makeQueryString(query);
+  return fetch(`${url}/${path}?${qs}`).then((res) => res.json());
+};
+const fetchDataById = (id, body) => {
+  return fetch(`${url}/discussions/${id}`, body).then((res) => res.text());
+};
+
 const makeQueryString = (obj) => {
   return Object.entries(obj)
     .map(([k, v]) => `${k}=${v}`)
     .join("&");
 };
 
-export { fetchData, makeQueryString };
+export { fetchData, makeQueryString, fetchDataWithQuery, fetchDataById };

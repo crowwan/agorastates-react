0808072377e163bd2ebaf5bceb32f16118934e8f
crowwan <crowwan@naver.com>
2023-02-09 14:40:38 +0900

    feat: initFetch 함수 useCallback 사용
    iniFetch 함수는 변할 이유가 없기 때문에 useCallback 사용
    자식 컴포넌트 렌더링에 영향은 없어서 추후에 원래 코드로 복원 가능성 있음

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import DiscussionsSection from "../components/DiscussionsSection";
 import Aside from "./Aside";
-import { fetchData } from "../utils/fetchData";
+import { fetchDataWithQuery } from "../utils/fetchData";
 
 export default function Main({ user }) {
   const [discussions, setDiscussions] = useState([]);
   const [pages, setPages] = useState(0);
   const [queryObj, setQueryObj] = useState({ page: 1 });
-  const initFetch = () => {
+
+  const initFetch = useCallback(() => {
     setQueryObj({ page: 1 });
-  };
+  }, []);
+
   useEffect(() => {
     (async () => {
       console.log("fetching...");
-      const res = await fetchData("discussions", { ...queryObj });
+      const res = await fetchDataWithQuery("discussions", { ...queryObj });
       setDiscussions(res.discussion);
       setPages(res.pages);
     })();

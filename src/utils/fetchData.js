898dfbc8fd449005aee9b17525fd7aadebcd48c4
crowwan <crowwan@naver.com>
2023-02-10 19:00:36 +0900

    feat, refactor: get이외의 fetch요청 처리 함수 추가, 이전 함수 코드 리펙토링

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,24 +1,16 @@
 const url = "http://localhost:4000";
 
-const fetchData = (path, query) =>
-  makeQueryString(query)
-    ? fetch(`${url}/${path}?${makeQueryString(query)}`).then((res) =>
-        res.json()
-      )
-    : fetch(`${url}/${path}`).then((res) => res.json());
+const fetchData = (path) => fetch(`${url}/${path}`).then((res) => res.json());
 
-const fetchDataWithQuery = (path, query) => {
-  const qs = makeQueryString(query);
-  return fetch(`${url}/${path}?${qs}`).then((res) => res.json());
-};
-const fetchDataById = (id, body) => {
-  return fetch(`${url}/discussions/${id}`, body).then((res) => res.text());
-};
+const fetchDataWithQuery = (path, query) =>
+  fetch(`${url}/${path}?${makeQueryString(query)}`).then((res) => res.json());
 
-const makeQueryString = (obj) => {
-  return Object.entries(obj)
+const fetchDataWithBody = (path, body) =>
+  fetch(`${url}/${path}`, body).then((res) => res.text());
+
+const makeQueryString = (obj) =>
+  Object.entries(obj)
     .map(([k, v]) => `${k}=${v}`)
     .join("&");
-};
 
-export { fetchData, makeQueryString, fetchDataWithQuery, fetchDataById };
+export { fetchData, makeQueryString, fetchDataWithQuery, fetchDataWithBody };

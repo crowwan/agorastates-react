2964c262d93d3b6538e5ac5bb00d5f55265d87ab
crowwan <crowwan@naver.com>
2023-02-25 10:18:24 +0900

    feat: 리덕스 적용으로 인한 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Header from "./layouts/Header";
 import Main from "./layouts/Main";
 import { storageAPI } from "./storage/storageAPI.js";
 function App() {
-  const [user, setUser] = useState("");
-
   useEffect(() => {
     storageAPI.setInitialStorage();
   }, []);
@@ -12,8 +10,8 @@ function App() {
     <div className="App">
       <div id="wrapper">
         <div className="container">
-          <Header user={user} setUser={setUser} />
-          <Main user={user} />
+          <Header />
+          <Main />
         </div>
       </div>
     </div>
